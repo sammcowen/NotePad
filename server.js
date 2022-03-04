8c3d378ccc5a9e991b4aefd928c9c80752a68c5a
HBK <sammcowen@gmail.com>
2022-03-03 23:36:18 -0800

    created home route and css file to be accessed without endpoint and get/notes route

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 
+const htmlroutes = require('./routes/htmlroutes/index');
+
+const path = require('path');
+
 const PORT = 3001;
 
 const app = express();
 
-// homepage route 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-});
 
+// middleware to attach and read stylesheet without specific endpoint
+app.use((express.static('public')));
+app.use('/', htmlroutes);
 app.listen(PORT, () => {
     console.log(`API server now  on ${PORT}`);
 });
